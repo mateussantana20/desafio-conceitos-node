1b0e096c4bb26106ed32b84539235cd65e0e813d
Jaime Barbosa <jaimebs@gmail.com>
2020-04-06 19:03:31 -0300

    feat: Criado regras de negocio para aplicação.

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
-const express = require("express");
-const cors = require("cors");
-
-// const { uuid } = require("uuidv4");
+const express = require('express');
+const cors = require('cors');
+const { uuid } = require('uuidv4');
 
 const app = express();
 
@@ -10,24 +9,75 @@ app.use(cors());
 
 const repositories = [];
 
-app.get("/repositories", (request, response) => {
-  // TODO
+app.get('/repositories', (req, res) => {
+  return res.status(200).json(repositories);
 });
 
-app.post("/repositories", (request, response) => {
-  // TODO
+app.post('/repositories', (req, res) => {
+  const { title, url, techs } = req.body;
+  const id = uuid();
+  const repository = {
+    id,
+    title,
+    url,
+    techs,
+    likes: 0,
+  };
+
+  repositories.push(repository);
+
+  return res.status(201).json(repository);
 });
 
-app.put("/repositories/:id", (request, response) => {
-  // TODO
+app.put('/repositories/:id', (req, res) => {
+  const { id } = req.params;
+  const { title, url, techs, likes } = req.body;
+
+  const respository = {
+    id,
+    title,
+    url,
+    techs,
+    likes,
+  };
+
+  const index = repositories.findIndex((repository) => repository.id === id);
+
+  if (index < 0) {
+    return res.status(400).json({ error: 'Repository not found!😢' });
+  }
+
+  repositories[index] = respository;
+
+  return res.status(200).json(respository);
 });
 
-app.delete("/repositories/:id", (req, res) => {
-  // TODO
+app.delete('/repositories/:id', (req, res) => {
+  const { id } = req.params;
+
+  const index = repositories.findIndex((repository) => repository.id === id);
+
+  if (index < 0) {
+    return res.status(400).json({ error: 'Repository not found!😢' });
+  }
+
+  repositories.splice(index, 1);
+
+  return res.status(204).send();
 });
 
-app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+app.post('/repositories/:id/like', (req, res) => {
+  const { id } = req.params;
+
+  const repository = repositories.find((repository) => repository.id === id);
+
+  if (!repository) {
+    return res.status(400).json({ error: 'Repository not found!😢' });
+  }
+
+  repository.likes += 1;
+
+  return res.status(200).json(repository);
 });
 
 module.exports = app;
